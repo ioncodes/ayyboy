a281703e4aba762c98e2c37a02e4bf49ca821e0c
Luca Marcelli <luca@marcelli.ch>
2024-06-22 15:39:36 +0200

    adjusted unit tests

--- src/tests.rs
+++ src/tests.rs
@@ -13,7 +13,7 @@ mod tests {
         input in r"^.*\.json" if !is_ignore
     })]
     fn test_cpu(input: &str) {
-        let tests: serde_json::Value = serde_json::from_str(&input).unwrap();
+        let tests: Value = serde_json::from_str(&input).unwrap();
 
         for test in tests.as_array().unwrap() {
             let mut mmu = Mmu::new(vec![], vec![0u8; 0xffff]);
@@ -22,6 +22,7 @@ mod tests {
             let mut cpu = Cpu::new();
 
             let test = test.as_object().unwrap();
+            let name = test.get("name").unwrap().as_str().unwrap();
             let initial = test.get("initial").unwrap().as_object().unwrap();
             let final_state = test.get("final").unwrap().as_object().unwrap();
 
@@ -55,52 +56,62 @@ mod tests {
             assert_eq!(
                 cpu.read_register(&Register::A),
                 final_state.get("a").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register A failed",
+                "Comparison with register A failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::F),
                 final_state.get("f").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register F failed"
+                "Comparison with register F failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::B),
                 final_state.get("b").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register B failed",
+                "Comparison with register B failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::C),
                 final_state.get("c").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register C failed",
+                "Comparison with register C failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::D),
                 final_state.get("d").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register D failed",
+                "Comparison with register D failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::E),
                 final_state.get("e").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register E failed",
+                "Comparison with register E failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::H),
                 final_state.get("h").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register H failed",
+                "Comparison with register H failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register(&Register::L),
                 final_state.get("l").unwrap().as_u64().unwrap() as u8,
-                "Comparison with register L failed",
+                "Comparison with register L failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register16(&Register::SP),
                 final_state.get("sp").unwrap().as_u64().unwrap() as u16,
-                "Comparison with register SP failed",
+                "Comparison with register SP failed for {}",
+                name
             );
             assert_eq!(
                 cpu.read_register16(&Register::PC),
                 final_state.get("pc").unwrap().as_u64().unwrap() as u16,
-                "Comparison with register PC failed",
+                "Comparison with register PC failed for {}",
+                name
             );
 
             let ram = final_state.get("ram").unwrap().as_array().unwrap();
@@ -108,7 +119,7 @@ mod tests {
                 let addr = value.as_array().unwrap()[0].as_u64().unwrap() as u16;
                 let value = value.as_array().unwrap()[1].as_u64().unwrap() as u8;
 
-                assert_eq!(mmu.read(addr), value, "Comparison with RAM failed");
+                assert_eq!(mmu.read(addr), value, "Comparison with RAM failed for {}", name);
             }
         }
     }
