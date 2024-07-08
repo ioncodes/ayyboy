625c7af3f7f2f69c4657f14149b8543dc6c67ab8
ioncodes <luca@marcelli.ch>
2024-07-08 21:28:34 +0200

    fixed mbc1 issues

--- src/memory/mapper/mbc1.rs
+++ src/memory/mapper/mbc1.rs
@@ -3,12 +3,18 @@ use crate::memory::mapper::Mapper;
 use crate::memory::{EXTERNAL_RAM_END, EXTERNAL_RAM_START};
 use log::{debug, warn};
 
-const RAM_ENABLE_RANGE: std::ops::RangeInclusive<u16> = 0x0000..=0x1fff;
-const SECONDARY_BANK_REGISTER: std::ops::RangeInclusive<u16> = 0x4000..=0x5fff;
-const ROM_BANK_RANGE: std::ops::RangeInclusive<u16> = 0x2000..=0x3fff;
-const ROM_SLOT_0_RANGE: std::ops::RangeInclusive<u16> = 0x0000..=0x3fff;
-const ROM_SLOT_1_RANGE: std::ops::RangeInclusive<u16> = 0x4000..=0x7fff;
-const BANKING_MODE_REGISTER: std::ops::RangeInclusive<u16> = 0x6000..=0x7fff;
+const RAM_ENABLE_START: u16 = 0x0000;
+const RAM_ENABLE_END: u16 = 0x1fff;
+const SECONDARY_BANK_REGISTER_START: u16 = 0x4000;
+const SECONDARY_BANK_REGISTER_END: u16 = 0x5fff;
+const ROM_BANK_START: u16 = 0x2000;
+const ROM_BANK_END: u16 = 0x3fff;
+const ROM_SLOT_0_START: u16 = 0x0000;
+const ROM_SLOT_0_END: u16 = 0x3fff;
+const ROM_SLOT_1_START: u16 = 0x4000;
+const ROM_SLOT_1_END: u16 = 0x7fff;
+const BANKING_MODE_START: u16 = 0x6000;
+const BANKING_MODE_END: u16 = 0x7fff;
 
 #[derive(Clone)]
 pub struct Mbc1 {
@@ -23,14 +29,12 @@ pub struct Mbc1 {
 
 impl Mbc1 {
     pub fn new(memory: Vec<u8>) -> Mbc1 {
-        // If the cart is not large enough to use the 2-bit register
-        // (≤ 8 KiB RAM and ≤ 512 KiB ROM) this mode select has no observable effect.
-        let secondary_banking_allowed = if memory.len() > 0x80000 { true } else { false };
+        let secondary_banking_allowed = memory.len() > 0x80000; // 512 KiB ROM
 
         Mbc1 {
             rom: memory,
             rom_bank: 1,
-            ram: vec![0; 0x2000],
+            ram: vec![0; 0x8000],
             ram_bank: 0,
             ram_enabled: false,
             banking_mode: false,
@@ -43,19 +47,19 @@ impl Mapper for Mbc1 {
     #[inline]
     fn read(&self, addr: u16) -> Result<u8, AyyError> {
         match addr {
-            addr if ROM_SLOT_0_RANGE.contains(&addr) => Ok(self.rom[addr as usize]),
-            addr if ROM_SLOT_1_RANGE.contains(&addr) => {
-                let addr = (addr as usize % 0x4000) + (self.rom_bank as usize * 0x4000);
-                Ok(self.rom[addr])
-            }
-            addr if addr >= EXTERNAL_RAM_START && addr <= EXTERNAL_RAM_END => {
-                if self.ram_enabled {
-                    let base_addr = (addr - EXTERNAL_RAM_START) as usize;
-                    let addr = base_addr + (self.ram_bank as usize * 0x2000);
-                    Ok(self.ram[addr])
+            ROM_SLOT_0_START..=ROM_SLOT_0_END => Ok(self.rom[addr as usize]),
+            ROM_SLOT_1_START..=ROM_SLOT_1_END => {
+                let rom_addr = (addr as usize % 0x4000) + (self.rom_bank as usize * 0x4000);
+                if rom_addr < self.rom.len() {
+                    Ok(self.rom[rom_addr])
                 } else {
                     Err(AyyError::OutOfBoundsMemoryAccess { address: addr })
                 }
+            }
+            EXTERNAL_RAM_START..=EXTERNAL_RAM_END if self.ram_enabled => {
+                let base_addr = (addr - EXTERNAL_RAM_START) as usize;
+                let ram_addr = base_addr + (self.ram_bank as usize * 0x2000);
+                Ok(self.ram[ram_addr])
             }
             _ => Err(AyyError::OutOfBoundsMemoryAccess { address: addr }),
         }
@@ -64,48 +68,38 @@ impl Mapper for Mbc1 {
     #[inline]
     fn write(&mut self, addr: u16, data: u8) -> Result<(), AyyError> {
         match addr {
-            addr if RAM_ENABLE_RANGE.contains(&addr) => {
+            RAM_ENABLE_START..=RAM_ENABLE_END => {
                 self.ram_enabled = (data & 0x0f) == 0x0a;
                 debug!("MBC1: RAM enabled: {}", self.ram_enabled);
             }
-            addr if ROM_BANK_RANGE.contains(&addr) => {
-                // This 5-bit register (range $01-$1F) selects the ROM bank number for the 4000–7FFF region.
-                // Higher bits are discarded — writing $E1 (binary 11100001) to this register would select bank $01.
+            ROM_BANK_START..=ROM_BANK_END => {
                 self.rom_bank = data & 0b0001_1111;
                 if self.rom_bank == 0 {
                     self.rom_bank = 1;
                 }
-                debug!("MBC1: Switched to RAM bank {}", self.ram_bank);
+                debug!("MBC1: Switched to ROM bank {}", self.rom_bank);
             }
-            addr if SECONDARY_BANK_REGISTER.contains(&addr) && !self.banking_mode => {
-                // This 5-bit register (range $01-$1F) selects the ROM bank number for the 4000–7FFF region.
-                // Higher bits are discarded — writing $E1 (binary 11100001) to this register would select bank $01.
-                if self.secondary_banking_allowed {
-                    self.ram_bank = data & 0b11;
-                    debug!("MBC1: Switched to RAM bank {}", self.rom_bank);
-                } else {
-                    warn!("MBC1: Attempted to switch to RAM bank, but not allowed");
-                }
-            }
-            addr if SECONDARY_BANK_REGISTER.contains(&addr) && self.banking_mode => {
-                // or to specify the upper two bits (bits 5-6) of the ROM Bank number (1 MiB ROM or larger carts only).
-                // If neither ROM nor RAM is large enough, setting this register does nothing.
+            SECONDARY_BANK_REGISTER_START..=SECONDARY_BANK_REGISTER_END if self.banking_mode => {
                 if self.secondary_banking_allowed {
                     self.rom_bank = (self.rom_bank & 0b0001_1111) | ((data & 0b11) << 5);
-                    debug!("MBC1: Switched to ROM bank {}", self.ram_bank);
+                    debug!("MBC1: Switched to ROM bank {}", self.rom_bank);
                 } else {
                     warn!("MBC1: Attempted to switch to ROM bank, but not allowed");
                 }
             }
-            addr if BANKING_MODE_REGISTER.contains(&addr) => {
+            SECONDARY_BANK_REGISTER_START..=SECONDARY_BANK_REGISTER_END if !self.banking_mode => {
+                self.ram_bank = data & 0b11;
+                debug!("MBC1: Switched to RAM bank {}", self.ram_bank);
+            }
+            BANKING_MODE_START..=BANKING_MODE_END => {
                 self.banking_mode = data & 0b0000_0001 == 1;
                 debug!("MBC1: Switched to banking mode: {}", self.banking_mode);
             }
-            addr if addr >= EXTERNAL_RAM_START && addr <= EXTERNAL_RAM_END => {
+            EXTERNAL_RAM_START..=EXTERNAL_RAM_END => {
                 if self.ram_enabled {
                     let base_addr = (addr - EXTERNAL_RAM_START) as usize;
-                    let addr = base_addr + (self.ram_bank as usize * 0x2000);
-                    self.ram[addr] = data;
+                    let ram_addr = base_addr + (self.ram_bank as usize * 0x2000);
+                    self.ram[ram_addr] = data;
                 } else {
                     return Err(AyyError::WriteToDisabledExternalRam { address: addr, data });
                 }
